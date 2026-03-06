95bfeba700fcf1a4768ec9d66690a136a00bca5b
dwinter1990 <dwinter1990@gmail.com>
2026-03-06 14:45:17 +1100

    PlantSelector
    Fixed plant selector script to only cycle through the seed options by one so that each type is accessible now.

--- Assets/Scripts/PlantSelector.cs
+++ Assets/Scripts/PlantSelector.cs
@@ -4,6 +4,11 @@ public class PlantSelector : MonoBehaviour
 {
     public void OnChangePlantType(InputAction.CallbackContext context)
     {
+        if(!context.performed) 
+        { 
+            return; 
+        }
+
         Vector2 scroll = context.ReadValue<Vector2>();
 
         if (scroll.y > 0.2f)
